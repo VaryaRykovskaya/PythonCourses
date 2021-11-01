c00aee5a2f0bdd6ffc36b4331702d52cc60ec7aa
VaryaRykovskaya <43346539+VaryaRykovskaya@users.noreply.github.com>
2021-11-01 22:27:19 +0300

    final version
    upload the final version of homework3

--- homework3.py
+++ homework3.py
@@ -22,20 +22,55 @@ class Car:
         self.cost = cst
         self.reg_num = regnum
 
-    def print_info(self):
-        print('id: ', self.car_id,
-              '\nМарка: ', self.brand,
-              '\nМодель: ', self.model,
-              '\nГод выпуска: ', self.year,
-              '\nЦвет: ', self.color,
-              '\nЦена: ', self.cost,
-              '\nРегистрационный номер: ', self.reg_num)
+    # def print_info(self):
+    #     print('id: ', self.car_id,
+    #           '\nМарка: ', self.brand,
+    #           '\nМодель: ', self.model,
+    #           '\nГод выпуска: ', self.year,
+    #           '\nЦвет: ', self.color,
+    #           '\nЦена: ', self.cost,
+    #           '\nРегистрационный номер: ', self.reg_num)
 
     def age_of_car(self):
         a = now.year - int(self.year)
-        print('Возраст машины: ', a)
+        # print('Возраст машины: ', a)
+        return a
+
+    def brand_print(self):
+        brnd_frm_keyboard = input("Введите марку авто: ")
+        for i in range (len(Spisok_Cars )):
+            brand_default = Spisok_Cars[i][1]
+            if brnd_frm_keyboard == brand_default:
+                print('..........................................................')
+                print('id: ', Spisok_Cars[i][0],
+                      '\nМарка: ', Spisok_Cars[i][1],
+                      '\nМодель: ', Spisok_Cars[i][2],
+                      '\nГод выпуска: ', Spisok_Cars[i][3],
+                      '\nЦвет: ', Spisok_Cars[i][4],
+                      '\nЦена: ', Spisok_Cars[i][5],
+                      '\nРегистрационный номер: ', Spisok_Cars[i][6],
+                      '\nВозраст машины: ', Spisok_Cars[i][7])
+                print('..........................................................')
+
+    def model_year_print(self):
+        model_frm_keyboard = input("Введите модель авто: ")
+        year_frm_keyboard = int(input("Введите число лет: "))
+        for i in range (len(Spisok_Cars)):
+            model_default = Spisok_Cars[i][2]
+            if (model_frm_keyboard == model_default) and (Spisok_Cars[i][7] > year_frm_keyboard):
+                print('*****************************************************************')
+                print('id: ', Spisok_Cars[i][0],
+                      '\nМарка: ', Spisok_Cars[i][1],
+                      '\nМодель: ', Spisok_Cars[i][2],
+                      '\nГод выпуска: ', Spisok_Cars[i][3],
+                      '\nЦвет: ', Spisok_Cars[i][4],
+                      '\nЦена: ', Spisok_Cars[i][5],
+                      '\nРегистрационный номер: ', Spisok_Cars[i][6],
+                      '\nВозраст машины: ', Spisok_Cars[i][7])
+                print('*****************************************************************')
 
 
+Spisok_Cars = []
 car = Car()
 Kol = int(input('Сколько машин добавить в список? '))
 for i in range(Kol):
@@ -51,8 +86,14 @@ for i in range(Kol):
     car.cost = input()
     print("Введите регистрационный номер: ")
     car.reg_num = input()
-    print('------------------------------')
-    car.print_info()
-    car.age_of_car()
-    print('===============================')
+    print('------------------------------------------------------------')
+    vozrast = car.age_of_car()
+    Spisok_Cars.append([car.car_id, car.brand, car.model, car.year, car.color, car.cost, car.reg_num, vozrast])
     car.car_id += 1
+print('Список всех машин: ', Spisok_Cars)
+print('==============================================================')
+car.brand_print()
+print('==============================================================')
+car.model_year_print()
+
+
