842480cc3243d3e479cebfd2825e4032c4dba2ac
VaryaRykovskaya <43346539+VaryaRykovskaya@users.noreply.github.com>
2021-11-11 23:42:25 +0300

    Update homework6_2.py

--- homework6_2.py
+++ homework6_2.py
@@ -3,11 +3,6 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait as wait
-from selenium.webdriver.support import expected_conditions
-
-
 
 class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
@@ -20,7 +15,7 @@ class PythonOrgSearch(unittest.TestCase):
         driver.get("http://automationpractice.com/index.php")
         # ждем 3 секунды
         time.sleep(3)
-        #получение элемента страницы с id search_query_top)
+        # #получение элемента страницы с id search_query_top)
         elem = driver.find_element_by_id("search_query_top")
         # ждем 3 секунды
         time.sleep(3)
@@ -44,7 +39,7 @@ class PythonOrgSearch(unittest.TestCase):
         elem.clear()
         #ждем 3 секунды
         time.sleep(3)
-        # набор слова pycon в найденном элементе
+        # набор слова blouse в найденном элементе
         elem.send_keys("blouse")
         # ждем 3 секунды
         time.sleep(3)
@@ -62,12 +57,6 @@ class PythonOrgSearch(unittest.TestCase):
         href_list = []
         for e in elems:
             href_list.append(e.get_attribute("href"))
-        # # перебираем полученные ссылки
-        # for i in range(len(href_list)):
-        #     # переходим по ссылке
-        #     driver.get(
-        #         href_list[i]
-        #     )
         driver.get(href_list[0])
         # ждем 3 секунды
         time.sleep(3)
@@ -102,29 +91,26 @@ class PythonOrgSearch(unittest.TestCase):
         href_list = []
         for e in elems:
             href_list.append(e.get_attribute("href"))
-        # # перебираем полученные ссылки
-        # for i in range(len(href_list)):
-        #     # переходим по ссылке
-        #     driver.get(
-        #         href_list[i]
-        #     )
         driver.get(href_list[1])
         # ждем 3 секунды
         time.sleep(3)
         elem = self.driver.find_element(By.ID, "wishlist_button")
         elem.click()
         time.sleep(3)
-
-        # elem = self.driver.find_element(By.ID, "image-block")
-        # elem.click()
-        # time.sleep(3)
-
-
-        # self.driver.find_element(By.CLASS_NAME, 'login')
-        # elem.click()
-        # time.sleep(3)
-
-
+        # Ищем кнопку крестик, которая свернёт окно о том, что нужно зарегистрироваться
+        elems = self.driver.find_elements(By.CLASS_NAME, 'fancybox-close')
+        # Если кнопка найдена, то кликаем на неё
+        if len(elems) != 0:
+            elems[0].click()
+        time.sleep(3)
+        # получение элемента страницы через css селектор)
+        elems = driver.find_elements_by_css_selector('.header_user_info .login')
+        href_list = []
+        for e in elems:
+            href_list.append(e.get_attribute("href"))
+        driver.get(href_list[0])
+        # ждем 3 секунды
+        time.sleep(3)
 
 def tearDown(self):
         # закрытие браузера при окончании каждого теста
